58d65a125dff36b02d0d71fa3549a5e97558ac4a
Dubymar Tollinchi <dubymarjtr@gmail.com>
2023-07-25 14:57:43 -0400

    import and use particles-bg

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
+import ParticlesBg from 'particles-bg'
 import Navigation from './components/Navigation/Navigation.js'
 import Logo from './components/Logo/Logo.js'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js'
 import Rank from './components/Rank/Rank.js'
 
+
 function App() {
   return (
     <div className="App">
+      <ParticlesBg className='particles' num={100} type="cobweb" bg={true}/>
       <Navigation />
       <Logo />
       <Rank />
