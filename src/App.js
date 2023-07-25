312e7b218ee650064992c417982ac18ea42b4262
Dubymar Tollinchi <dubymarjtr@gmail.com>
2023-07-25 13:55:55 -0400

    create Rank component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Navigation from './components/Navigation/Navigation.js'
 import Logo from './components/Logo/Logo.js'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js'
+import Rank from './components/Rank/Rank.js'
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/* <FaceRecognition /> */}
     </div>
