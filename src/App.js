787c7a9b30039acd8cf0790f74d07e630dcdf30b
Dubymar Tollinchi <dubymarjtr@gmail.com>
2023-07-25 13:42:19 -0400

    create ImageLinkForm component and style it

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import Navigation from './components/Navigation/Navigation.js'
 import Logo from './components/Logo/Logo.js'
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js'
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Logo />
-      {/* <ImageLinkForm />
-      <FaceRecognition /> */}
+      <ImageLinkForm />
+      {/* <FaceRecognition /> */}
     </div>
   );
 }
